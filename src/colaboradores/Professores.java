d21ef0c5894ae354438c647652c12db0fcc63a1f
Kelvin Almeida <kelvinsantosdealmeida@gmail.com>
2020-12-02 09:18:32 -0300

    update

--- src/colaboradores/Professores.java
+++ src/colaboradores/Professores.java
@@ -16,6 +16,9 @@ public class Professores extends Colaboradores {
     }
 
     public void mostrarOrientacao() {
+
+        if(ConjOrientacao.size() > 1) this.ordenarOrientacao();
+
         System.out.println("## ORIENTACOES:");
 
         for(int i = 0; i < ConjOrientacao.size(); ++i) {
@@ -23,6 +26,24 @@ public class Professores extends Colaboradores {
             System.out.println("## Orientacao " + "["+(i+1)+"] Projeto: " + oriEsco.getProj().getTitulo() + "  Ano: " + oriEsco.getAno());
             System.out.println("## Nota: " + oriEsco.getOri());
             System.out.println("## ----------------------------- ");
+        }
+    }
+
+    public void ordenarOrientacao() {
+        for (int j = 0; j < ConjOrientacao.size(); j++) {
+            for (int i = 0; i < (ConjOrientacao.size() - 1); i++) {
+                Orientacao ori1 = ConjOrientacao.get(i);
+                Orientacao ori2 = ConjOrientacao.get(i+1);
+                Orientacao ori3;
+                
+                if(ori1.getAno() < ori2.getAno()) {
+                    ori3 = ori1;
+                    ConjOrientacao.remove(i);
+                    ConjOrientacao.add(i, ori2);
+                    ConjOrientacao.remove(i+1);
+                    ConjOrientacao.add((i+1), ori3);
+                }
+            }
         }
     }
 
